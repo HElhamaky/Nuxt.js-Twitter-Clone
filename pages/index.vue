ecbfd622a902d1ad2588f59e7067debc25ea8ab8
HElhamaky <hassanelhamaky.social@gmail.com>
2023-01-15 13:10:54 +0200

    testing vercel auto build

--- pages/index.vue
+++ pages/index.vue
@@ -1,47 +1,44 @@
 <template>
-    <div>
-        <MainSection title="Home" :loading="loading">
+  <div>
+    <MainSection title="Home" :loading="loading">
+      <Head>
+        <Title>Home / FaceTwitter</Title>
+      </Head>
 
-            <Head>
-                <Title>Home / Twitter</Title>
-            </Head>
+      <div class="border-b" :class="twitterBorderColor">
+        <TweetForm :user="user" @on-success="handleFormSuccess" />
+      </div>
 
-            <div class="border-b" :class="twitterBorderColor">
-                <TweetForm :user="user" @on-success="handleFormSuccess" />
-            </div>
-
-            <TweetListFeed :tweets="homeTweets" />
-
-        </MainSection>
-    </div>
+      <TweetListFeed :tweets="homeTweets" />
+    </MainSection>
+  </div>
 </template>
 <script setup>
-const { twitterBorderColor } = useTailwindConfig()
-const { getTweets } = useTweets()
+const { twitterBorderColor } = useTailwindConfig();
+const { getTweets } = useTweets();
 
-const loading = ref(false)
-const homeTweets = ref([])
-const { useAuthUser } = useAuth()
+const loading = ref(false);
+const homeTweets = ref([]);
+const { useAuthUser } = useAuth();
 
-const user = useAuthUser()
+const user = useAuthUser();
 
 onBeforeMount(async () => {
-    loading.value = true
-    try {
-        const { tweets } = await getTweets()
+  loading.value = true;
+  try {
+    const { tweets } = await getTweets();
 
-        homeTweets.value = tweets
-    } catch (error) {
-        console.log(error)
-    } finally {
-        loading.value = false
-    }
-})
+    homeTweets.value = tweets;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    loading.value = false;
+  }
+});
 
 function handleFormSuccess(tweet) {
-    navigateTo({
-        path: `/status/${tweet.id}`
-    })
+  navigateTo({
+    path: `/status/${tweet.id}`,
+  });
 }
-
 </script>
